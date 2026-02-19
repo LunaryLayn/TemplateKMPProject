3e720395879d086c75e8d785e0dd29b1b250b871
LunaryLayn <hupogo2000@protonmail.com>
2026-02-19 18:55:53 +0100

    Added root to iOS app

--- composeApp/src/iosMain/kotlin/com/astrocoders/cooki/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/astrocoders/cooki/MainViewController.kt
@@ -1,9 +1,17 @@
 package com.astrocoders.cooki
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.astrocoders.cooki.ui.app.initKoin
+import com.astrocoders.cooki.ui.navigation.RootComponent
 
 fun MainViewController() = ComposeUIViewController {
     initKoin()
-    App()
+    val lifecycle = LifecycleRegistry()
+    val componentContext = DefaultComponentContext(lifecycle)
+
+    val root = RootComponent(componentContext)
+
+    App(root)
 }
